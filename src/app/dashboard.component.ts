57ba196c940191d6c274d59f73ec631310cf8277
Eduardo Santiago <eeduardosantiago@gmail.com>
2017-03-21 20:05:52 -0300

    conflict

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -5,6 +5,15 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-dashboard',
-  templateUrl: './dashboard.component.html',
+ // templateUrl: './dashboard.component.html',
+ template: `
+  <h3>Top Heroes</h3>
+  <div class="grid grid-pad">
+    <div *ngFor="let hero of heroes" class="col-1-4">
+    <div class="module hero">
+        <h4>{{hero.name}}</h4>
+    </div>
+    </div>
+  </div>`
 })
 export class DashboardComponent { }
